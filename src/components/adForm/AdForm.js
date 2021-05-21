1eb6bbc8eee9178f96c7e8e63493f3fdc393dde8
RafaelVi <rafaelalmeida383@gmail.com>
2021-05-21 14:57:56 -0300

    Sign In and Sign Up implemented, need to work in redirection

---
+++ src/components/adForm/AdForm.js
@@ -0,0 +1,60 @@
+import {useState} from "react";
+import { Link } from "react-router-dom";
+import HighlightButton from "../buttons/HighlightButton";
+import Logo from "../../images/logo-doarte.png";
+import { Form, Container } from "./AdForm.styles";
+import { createUser,loginUser } from "../../services/userServices";
+
+function AdForm({ signUp }) {
+  
+    const [user, setUser] = useState({
+      name: "",
+      email: "",
+      password: "",
+    });
+  
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    signUp?createUser(user):loginUser(user);
+  };
+  const takeValue = e =>{
+    const {id,value} = e.target;
+    setUser(prevState =>({
+      ...prevState,
+      [id]:value
+    }))
+    
+  }
+
+  return (
+    <Container>
+      <Form method="POST" onSubmit={handleSubmit}>
+        <img src={Logo} alt="DOARTE" />
+
+        {signUp && (
+          <input type="text" placeholder="Nome completo" id="name" onBlur={takeValue} required />
+        )}
+
+        <input type="email" placeholder="Endereço de e-mail" id="email" onBlur={takeValue} required/>
+        <input type="password" placeholder="Senha" id="password" onBlur={takeValue} required />
+
+        {signUp ? (
+          <HighlightButton type={"submit"} text={"Cadastrar-me"} />
+        ) : (
+          <HighlightButton type={"submit"} text={"Entrar"} />
+        )}
+
+        <hr />
+
+        {signUp ? (
+          <Link to="/signIn">Fazer login</Link>
+        ) : (
+          <Link to="/signUp">Criar conta grátis</Link>
+        )}
+      </Form>
+    </Container>
+  );
+}
+
+export default AdForm;
