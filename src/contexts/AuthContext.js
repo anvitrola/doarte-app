7be1a66559c9a005304f2017c2b690995d94f996
RafaelVi <rafaelalmeida383@gmail.com>
2021-05-22 23:56:03 -0300

    Auth commented while not properly configured

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,45 +1,45 @@
-import React, {createContext, useState, useEffect} from 'react';
-import api from '../services/api';
+// import React, {createContext, useState, useEffect} from 'react';
+// import {api} from '../services/api';
 
-const Context = createContext();
+// const Context = createContext();
 
-function AuthProvider({children}){
-    const {authenticated, setAuthenticated} = useState(false);
-    const {loading, setLoading} = useState(true);
+// function AuthProvider({children}){
+//     const {authenticated, setAuthenticated} = useState(false);
+//     const {loading, setLoading} = useState(true);
 
-    useEffect(() => {
-        const token = localStorage.getItem('token');
+//     useEffect(() => {
+//         const token = localStorage.getItem('token');
 
-        if(token) {
-            api.defaults.headers.Authorization = `Bearer ${JSON.parse(token)}`;
-            setAuthenticated(true);
-        } 
+//         if(token) {
+//             api.defaults.headers.Authorization = `Bearer ${JSON.parse(token)}`;
+//             setAuthenticated(true);
+//         } 
 
-        setLoading(false)
+//         setLoading(false)
 
-    }, []);
+//     }, []);
 
-    function handleAuth(data){
-        const {token} = data;
+//     function handleAuth(data){
+//         const {token} = data;
 
-        localStorage.setItem('token', JSON.stringify(token));
+//         localStorage.setItem('token', JSON.stringify(token));
 
-        api.defaults.headers.Authorization = `Bearer ${token}`;
+//         api.defaults.headers.Authorization = `Bearer ${token}`;
 
-        setAuthenticated(true);
-    }
+//         setAuthenticated(true);
+//     }
 
-    /**function handleLogout(){
-        setAuthenticated(false);
-        localStorage.removeItem('token');
-        api.defaults.headers.Authorization = undefined;
-    }**/
+//     /**function handleLogout(){
+//         setAuthenticated(false);
+//         localStorage.removeItem('token');
+//         api.defaults.headers.Authorization = undefined;
+//     }**/
 
-    return(
-        <Context.Provider value={{authenticated, handleAuth}}>
-            {children}
-        </Context.Provider>
-    );
-};
+//     return(
+//         <Context.Provider value={{authenticated, handleAuth}}>
+//             {children}
+//         </Context.Provider>
+//     );
+// };
 
-export {Context, AuthProvider};
+// export {Context, AuthProvider};
